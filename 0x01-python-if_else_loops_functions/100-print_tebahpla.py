6fc6ebee78cfb5c4f1c3009d7b573b2dac3d1d29
BoakyePrincella <princellaboakye2003@gmail.com>
2024-03-12 16:39:38 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,6 @@
 
 def print_reverse_alphabet():
     for ascii_value in range(ord('z'), ord('a') - 1, -1):
-        print("{:c}".format(ascii_value - (ord('a') - ord('A')) * ((ascii_value - ord('z')) % 2)), end=' ')
+        print("{:c}".format(ascii_value - (ord('a') - ord('A')) * ((ascii_value - ord('z')) % 2)), end='')
 
 print_reverse_alphabet()
-
