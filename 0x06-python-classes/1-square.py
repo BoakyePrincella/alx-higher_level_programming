93cbd4c714e7614e0b213e010b5ff735a6639411
BoakyePrincella <princellaboakye2003@gmail.com>
2024-04-02 17:26:03 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''A sqaure class with size'''
 
-def __init__(self, size):
-    self.__size = size
+
+class Square:
+	'''Defining a class square'''
+	def __init__(self, size):
+		'''Initiating a square class
+		Args: size: size of the square
+		'''
+		self.__size = size
