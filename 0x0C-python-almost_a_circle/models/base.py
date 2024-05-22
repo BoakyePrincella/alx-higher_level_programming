4108b96c6fdb1ef0bebee73810a6795dd8360416
BoakyePrincella <princellaboakye2003@gmail.com>
2024-05-22 14:02:59 -0700

    Models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""This module contains classes that perform specific functions"""
+
+
+class Base:
+    """This is the base class defined that manages id"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor or initializer"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Represent the json representation"""
+        import json
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.JSONEncoder().encode(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the json representation of @list_objs to a file"""
+        import json
+        with open(f'{cls.__name__}.json', 'w') as f:
+            list_dictionary = []
+            if list_objs:
+                for obj in list_objs:
+                    list_dictionary.append(obj.to_dictionary())
+            f.write(cls.to_json_string(list_dictionary))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the json string representation"""
+        import json
+        if json_string is None:
+            return []
+        else:
+            return json.JSONDecoder().decode(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """This creates a new instance of the calling class"""
+        temp = None
+        if cls.__name__ == "Rectangle":
+            temp = cls(2, 3)
+        elif cls.__name__ == "Square":
+            temp = cls(4)
+        temp.update(**dictionary)
+        return temp
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads a list of instances from a file"""
+        filename = cls.__name__ + ".json"
+        try:
+            res = []
+            with open(filename, "r") as f:
+                list_dict = cls.from_json_string(f.read())
+                for dic in list_dict:
+                    res.append(cls.create(**dic))
+                return res
+        except Exception:
+            return []
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""This module has one class that inherits from base"""
+
+
+from .base import Base
+
+
+class Rectangle(Base):
+    """This is the Rectangle class that inherits from the Base class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """The initializer or constructor function"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """The setter and getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """The setter and getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """The setter and getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """The setter and getter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """The area of the Rectangle instance"""
+        return self.height * self.width
+
+    def display(self):
+        """Prints the Rectangle with #"""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    def __str__(self):
+        """String representation of Rectangle instance"""
+        rect_name = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return rect_name
+
+    def update(self, *args, **kwargs):
+        """The update method of the Rectangle"""
+        attributes = ['id', 'width', 'height', 'x', 'y']
+        if args:
+            for i, arg in enumerate(args):
+                if i == 5:
+                    break
+                setattr(self, attributes[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key in attributes:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """The dictionary representation of the Rectangle class"""
+        dict_rep = {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
+        return dict_rep
