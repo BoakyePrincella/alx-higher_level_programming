266ada06f803788ba9e3a08bbfaf99b9802aaa80
BoakyePrincella <princellaboakye2003@gmail.com>
2024-05-27 12:09:45 +0000

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
--- lists the number of records with the same scores
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+-- looks for records with names
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
