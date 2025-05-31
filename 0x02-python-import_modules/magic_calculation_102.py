caf97966d8115c9191837f5a7cca121b8e7ce608
BoakyePrincella <boakyeprincella2003@gmail.com>
2025-05-31 21:34:21 +0000

    Modules

---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module providing basic arithmetic functions."""
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return a + b
+
+
+def sub(a, b):
+    """Return the subtraction of b from a."""
+    return a - b
+
