662c5e9222ed11cdd1f7b205495c42037766f846
BoakyePrincella <boakyeprincella2003@gmail.com>
2025-06-02 22:14:49 +0000

    Python Classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Creating a square class """
+
+
+class Square:
+    """ Defining a class square """
+    def __init__(self, size=0):
+        """ Initializing a square class
+        Args: size=0: size of the square
+         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Calculating the area of the square """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Initialising a property attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Iniatialising a setter """
+        self.__radius = value
